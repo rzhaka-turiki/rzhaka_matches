9dbb3b8b21b1be91f08c1b1621ba07ba5db47627
a1uka <88232045+a1uka@users.noreply.github.com>
2026-07-08 18:43:23 +0300

    debug things

--- api/main_api.cpp
+++ api/main_api.cpp
@@ -1,5 +1,6 @@
 #include <grpcpp/grpcpp.h>
 #include <spdlog/spdlog.h>
+#include <grpcpp/ext/reflection.h>
 #include "grpc_service.h"
 #include "config.h"
 #include "database.h"
@@ -21,6 +22,8 @@ int main(int argc, char* argv[]) {
     builder.AddListeningPort("0.0.0.0:50051", grpc::InsecureServerCredentials());
     builder.RegisterService(&service);
     std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+    grpc::reflection::InitDefaultReflection();
+    spdlog::info("gRPC reflection enabled");
     spdlog::info("gRPC server listening on 0.0.0.0:50051");
 
     signal(SIGINT, [](int) { running = false; });
