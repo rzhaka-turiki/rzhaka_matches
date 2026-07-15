3a788b828a57dec5719937ee0592f5ca27f34684
a1uka <88232045+a1uka@users.noreply.github.com>
2026-07-15 10:47:05 +0300

    added matches cache

--- src/match_processor.h
+++ src/match_processor.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <pqxx/pqxx>
 #include <string>
+#include <unordered_set>
 
 #include "api_client.h"
 
@@ -13,6 +14,9 @@ private:
     std::string base_url_;
 
     void parse_and_store(int token_id, const std::string& json_response);
+    // mids hash
+    std::unordered_set<std::string> stored_mids_;
+    std::mutex mids_mutex_;
 
 public:
     MatchProcessor(pqxx::connection& conn, const ApiClient& api, const std::string& base_url);
