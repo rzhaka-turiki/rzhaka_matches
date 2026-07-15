d2b97bfb8c8ffc32d69488a8738054595a9096a3
a1uka <88232045+a1uka@users.noreply.github.com>
2026-07-15 04:39:09 +0300

    small fix

--- src/match_processor.h
+++ src/match_processor.h
@@ -7,15 +7,15 @@
 struct ActiveToken;
 
 class MatchProcessor {
-public:
-    MatchProcessor(pqxx::connection& conn, const ApiClient& api, const std::string& base_url);
-
-    void process_token(const ActiveToken& token);
-
 private:
     pqxx::connection& conn_;
     const ApiClient& api_;
     std::string base_url_;
 
     void parse_and_store(int token_id, const std::string& json_response);
+
+public:
+    MatchProcessor(pqxx::connection& conn, const ApiClient& api, const std::string& base_url);
+
+    void process_token(const ActiveToken& token);
 };
