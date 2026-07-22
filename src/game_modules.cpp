a9f34aafe82dda205196e5c0438e8676719ac02d
aluka <88232045+a1uka@users.noreply.github.com>
2026-07-22 15:26:15 +0300

    typo fix

--- src/game_modules.cpp
+++ src/game_modules.cpp
@@ -7,7 +7,7 @@ void Player::init(nlohmann::json s_json_) {
     team_num_ = s_json_.value("teamNum", 0);
     team_placement_ = s_json_.value("teamPlacement", 0);
     character_name_ = s_json_.value("characterName", "");
-    hardware_ = s_json_.value("hradware", "");
+    hardware_ = s_json_.value("hardware", "");
     kills_ = s_json_.value("kills", 0);
     assists_ = s_json_.value("assists", 0);
     knockdowns_ = s_json_.value("knockdowns", 0);
